d89eac774890482fd848715154f5a9cd50bf5a25
Sam Puttman <daputter2@gmail.com>
2024-04-23 12:19:27 -0400

    add sessions#create to routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
       get '/book-search', to: 'book_search#index'
 
       resources :users, only: [:create]
-
+      resources :sessions, only: [:create]
     end
   end
 end
